f9429e2d8afdc469a2e1c967960b5556d53570fd
jsntxyang <83987249+jsntxyang@users.noreply.github.com>
2021-11-12 18:32:52 +0000

    Add files via upload

--- STM_plot.py
+++ STM_plot.py
@@ -1,5 +1,5 @@
 import numpy as np
-import matplotlib.pyplot as pl
+import pylab as pl
 def select_region(data, aperiod, bperiod, cell):
     size = data.shape
     a_size = size[0]
@@ -30,11 +30,16 @@ def select_region(data, aperiod, bperiod, cell):
     return data_new, a, b
 
 
-def contour_plot(data, aperiod, bperiod, cell, figure_name=0):
+def contour_plot(data, aperiod, bperiod, cell, figure_name=0, mode='auto', vmin=0, vmax=1):
     new_data, X, Y = select_region(data=data, aperiod=aperiod, bperiod=bperiod, cell=cell)
 
     fig = pl.figure(figure_name)
     ax = fig.add_subplot(111, adjustable='box', aspect=1)
     level = np.linspace(0, 5, 100)
-    fig.colorbar(ax.contourf(X, Y, new_data, 100, level=level))
+
+    if mode == 'auto':
+        fig.colorbar(ax.contourf(X, Y, new_data, 200, level=level))
+    elif mode == 'absolute':
+        fig.colorbar(ax.contourf(X, Y, new_data, 200, level=level, vmin=vmin, vmax=vmax))
+
     return fig, ax
